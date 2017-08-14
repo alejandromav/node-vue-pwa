3df5edc6663a935c6067c9794e6c6b00af854eab
Alejandro Martin <alejandromav@ext.inditex.com>
2017-08-14 12:26:17 +0200

    Menu button usability fix

--- public/src/event-bus.js
+++ public/src/event-bus.js
@@ -2,10 +2,12 @@ import Vue from 'vue';
 
 const EventBus = new Vue();
 
-export default EventBus;
-
 // EventBus.$emit('i-got-clicked', this.clickCount);
 
 // EventBus.$on('i-got-clicked', clickCount => {
 //     console.log(`Oh, that's nice. It's gotten ${clickCount} clicks! :)`)
 // });
+//
+//
+
+export default EventBus;
